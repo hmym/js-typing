dce56b05cccb291a6a376a43bbce9d219135ab9d
hmym <chika.hamayama@gmail.com>
2019-04-05 22:16:18 +0900

    add updateTarget

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 'use strict';
 
 {
-  const word = 'apple';
+  const words = [
+    'apple','sky','blue','middle','set'
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
-
+  let score = 0;
+  let miss = 0;
   const target = document.getElementById('target');
-  target.textContent = word;
+  const scoreLavel = document.getElementById('score');
+  const missLavel = document.getElementById('miss');
+
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
+  window.addEventListener('click', () => {
+    updateTarget();
+  });
 
   window.addEventListener('keyup', e => {
-    console.log(e.key);
     if (e.key === word[loc]) {
-      console.log('score');
       loc++
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      score++
+      scoreLavel.textContent = score;
+      updateTarget();
     } else {
-      console.log('miss');
+      miss++
+      missLavel.textContent = miss;
     }
   });
 }
